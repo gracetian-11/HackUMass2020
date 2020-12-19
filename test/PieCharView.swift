c9d56b0d1fa70cb8554acb80fb564aeef3baa6d8
Kerry Lu <kerry.lu@duke.edu>
2020-12-18 16:51:43 -0800

    Initial commit

---
+++ test/PieCharView.swift
@@ -0,0 +1,20 @@
+//
+//  PieCharView.swift
+//  test
+//
+//  Created by kerry lu on 12/18/20.
+//
+
+import UIKit
+
+class PieCharView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
