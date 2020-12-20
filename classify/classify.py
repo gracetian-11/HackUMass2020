b70bc980ff2c75bc805a5e20e70fa10011c9dd70
Grace Tian <grace.tian@duke.edu>
2020-12-19 17:17:35 -0800

    receipt parser

--- classify/classify.py
+++ classify/classify.py
@@ -34,3 +34,35 @@ def classify(text, verbose=True):
             print(u"{:<16}: {}".format("confidence", category.confidence))
     print("HELLO" + result)
     return result
+
+if __name__ == "__main__":
+    classify("""
+    Walmart
+    Save money. Live better.
+    (526) - 237-0154
+    MANAGER TONY ESCARIO
+    DEATH VALLEY, OU
+    ST: 1088 OP# 00009045 TE# 45 TR# 08570
+    AIDS
+    002840001408 51.74 X
+    GAMECOM X40 0074100514919 2.50 X
+    PEPSI 001200080994 4,38 R
+    SUBTOTAL 12.84
+    TAX 1 8.875
+    5,20
+    TOTAL
+    63.82
+    DEBIT TEND 63.82
+    EFT DEBIT PAY FROM PRIMARY
+    ACOUNT # olek Holok Selek 7428 S
+    APPROVAL # 001824
+    REF # 224500487251
+    TERMINAL # 52084660
+    09/01/12 14:16:00
+    CHANGE DE 0.00
+    # ITEMS SOLD 3
+    TCH 7884 0145 2857 6283 1227
+    "Like" Walmart on Facebook
+    www.facebook.com/Walmart
+    09/01/12 14:16:00
+    """)
---
+++ receipt_parser/receipt_parser.py
@@ -0,0 +1,81 @@
+import os
+from google.cloud import documentai_v1beta3 as documentai
+
+project_id= 'respend'
+location = 'us' # Format is 'us' or 'eu'
+processor_id = 'b59cbc5119e37d4e' # Create processor in Cloud Console
+file_path = './receipt_parser/test_receipt.pdf'
+
+def process_document_sample(
+    project_id: str, location: str, processor_id: str, file_path: str
+):
+
+    # Instantiates a client
+    client = set_client()
+
+    # The full resource name of the processor, e.g.:
+    # projects/project-id/locations/location/processor/processor-id
+    # You must create new processors in the Cloud Console first
+
+    # https://us-documentai.googleapis.com/v1beta3/projects/251887149747/locations/us/processors/b59cbc5119e37d4e:process 
+    name = f"projects/{project_id}/locations/{location}/processors/{processor_id}"
+
+    with open(file_path, "rb") as image:
+        image_content = image.read()
+
+    # Read the file into memory
+    document = {"content": image_content, "mime_type": "application/pdf"}
+
+    # Configure the process request
+    request = {"name": name, "document": document}
+
+    # Recognizes text entities in the PDF document
+    result = client.process_document(request=request)
+
+    document = result.document
+
+    print("Document processing complete.")
+
+    # For a full list of Document object attributes, please reference this page: https://googleapis.dev/python/documentai/latest/_modules/google/cloud/documentai_v1beta3/types/document.html#Document
+
+    document_pages = document.pages
+
+    # Read the text recognition output from the processor
+    print("The document contains the following paragraphs:")
+    for page in document_pages:
+        paragraphs = page.paragraphs
+        for paragraph in paragraphs:
+            paragraph_text = get_text(paragraph.layout, document)
+            print(f"Paragraph text: {paragraph_text}")
+
+
+# Extract shards from the text field
+def get_text(doc_element: dict, document: dict):
+    """
+    Document AI identifies form fields by their offsets
+    in document text. This function converts offsets
+    to text snippets.
+    """
+    response = ""
+    # If a text segment spans several lines, it will
+    # be stored in different text segments.
+    for segment in doc_element.text_anchor.text_segments:
+        start_index = (
+            int(segment.start_index)
+            if segment in doc_element.text_anchor.text_segments
+            else 0
+        )
+        end_index = int(segment.end_index)
+        response += document.text[start_index:end_index]
+    return response
+
+def set_client():
+    # setup credential and create a vision client
+    # TODO: make sure to update this to match the path of gcp credential file
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/gracetian/Desktop/ReSpend/respend-9a49e28166f6.json"
+
+    client = documentai.DocumentProcessorServiceClient()
+    return client
+
+if __name__ == "__main__":
+    process_document_sample(project_id, location, processor_id, file_path)
